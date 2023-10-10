9fdbad853aa942b8e2a3392a7e6c614d67e434a6
saraiva142 <saraivajandrade@gmail.com>
2023-10-09 21:15:02 -0300

    Multi ok

--- client.py
+++ client.py
@@ -16,13 +16,16 @@ async def send_message(message):
     await writer.wait_closed()
 """
 
+dados_exemplos = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+"""
 async def enviar_dados(reader, writer):
     sensor_id = writer.get_extra_info('peername')
     print(f"Conexão de sensor estabelecida com {sensor_id}")
 
     while True:
         # Simula a leitura de dados do sensor (gera um valor aleatório)
-        dados_sensor = random.uniform(0.0, 100.0)
+        dados_sensor = random.choice(dados_exemplos)
         mensagem = f"Dado do sensor {sensor_id}: {dados_sensor}"
 
         # Envia os dados para o servidor
@@ -33,8 +36,46 @@ async def enviar_dados(reader, writer):
 
         # Aguarda um intervalo de tempo antes de enviar o próximo dado
         await asyncio.sleep(random.uniform(1, 5))
+"""
 
 
+async def enviar_dados(sensor_id, server_port):
+    reader, writer = await asyncio.open_connection('127.0.0.1', server_port)
+
+    print(f"Conexão com Sensor {sensor_id} estabelecida")
+
+    while True:
+        # Simula a leitura de dados do sensor (gera um valor aleatório)
+        dados_sensor = random.uniform(0.0, 100.0)
+        mensagem = f"Dado do sensor {sensor_id}: {dados_sensor}"
+
+        # Envia os dados para o servidor do sensor correspondente
+        writer.write(mensagem.encode())
+        await writer.drain()
+
+        print(f"Dados enviados para Sensor {sensor_id}: {mensagem}")
+
+        # Aguarda um intervalo de tempo antes de enviar o próximo dado
+        await asyncio.sleep(random.uniform(1, 5))
+        
+async def main():
+    # Define os IDs e portas dos sensores
+    sensores = [
+        {"id": "Sensor-1", "port": 8888},
+        {"id": "Sensor-2", "port": 8889},
+        {"id": "Sensor-3", "port": 8890},
+    ]
+
+    # Inicia tarefas para conectar e enviar dados para cada sensor
+    for sensor in sensores:
+        asyncio.create_task(enviar_dados(sensor["id"], sensor["port"]))
+
+    while True:
+        await asyncio.sleep(1)
+
+asyncio.run(main())
+
+"""
 async def main():
     
     num_sensores = 3  # Define o número de sensores (clientes) que deseja iniciar
@@ -54,3 +95,4 @@ async def main():
       #await send_message("Olá de novo, Servidor!")
 
 asyncio.run(main())
+"""
--- server.py
+++ server.py
@@ -18,8 +18,11 @@ async def handle_echo(reader, writer):
 
 # Lista para armazenar os dados dos sensores
 dados_do_sensor_1 = []
+dados_do_sensor_2 = []
+dados_do_sensor_3 = []
 
-async def gerenciar_sensor(reader, writer):
+
+async def gerenciar_sensor_1(reader, writer):
     addr = writer.get_extra_info('peername')
     print(f"Conexão de sensor estabelecida com {addr}")
 
@@ -29,24 +32,80 @@ async def gerenciar_sensor(reader, writer):
             break
 
         mensagem = data.decode()
-        print(f"Dados recebidos do sensor {addr}: {mensagem}")
+        print(f"Dados recebidos do sensor 1 {addr}: {mensagem}")
 
         # Armazena os dados do sensor na lista
         dados_do_sensor_1.append((addr, mensagem))
 
         # Responde ao sensor (opcional)
-        response = "Dados recebidos com sucesso!"
+        response = "Dados do Sensor 1 recebidos com sucesso!"
         writer.write(response.encode())
         await writer.drain()
 
-    print(f"Fechando a conexão com {addr}")
+    print(f"Fechando a conexão com Sensor 1 {addr}")
+    writer.close()
+    
+# Função para gerenciar o sensor 2
+async def gerenciar_sensor_2(reader, writer):
+    addr = writer.get_extra_info('peername')
+    print(f"CONEXÃO DE SESNOR 2 ESTABELECIDA COM {addr}")
+
+    while True:
+        data = await reader.read(100)
+        if not data:
+            break
+
+        mensagem = data.decode()
+        print(f"Dados recebidos do Sensor 2 {addr}: {mensagem}")
+
+        # Armazena os dados do Sensor 2 na lista
+        dados_do_sensor_2.append((addr, mensagem))
+
+        # Responde ao Sensor 2 (opcional)
+        response = "Dados do Sensor 2 recebidos com sucesso!"
+        writer.write(response.encode())
+        await writer.drain()
+
+    print(f"Fechando a conexão com Sensor 2 {addr}")
     writer.close()
 
+# Função para gerenciar o sensor 3
+async def gerenciar_sensor_3(reader, writer):
+    addr = writer.get_extra_info('peername')
+    print(f"Conexão de Sensor 3 estabelecida com {addr}")
+
+    while True:
+        data = await reader.read(100)
+        if not data:
+            break
+
+        mensagem = data.decode()
+        print(f"Dados recebidos do Sensor 3 {addr}: {mensagem}")
+
+        # Armazena os dados do Sensor 3 na lista
+        dados_do_sensor_3.append((addr, mensagem))
+
+        # Responde ao Sensor 3 (opcional)
+        response = "Dados do Sensor 3 recebidos com sucesso!"
+        writer.write(response.encode())
+        await writer.drain()
+
+    print(f"Fechando a conexão com Sensor 3 {addr}")
+    writer.close()
 
 async def main():
-    server = await asyncio.start_server(gerenciar_sensor, '127.0.0.1', 8888)
-    async with server:
+    #server = await asyncio.start_server(gerenciar_sensor, '127.0.0.1', 8888)
+    server_1 = await asyncio.start_server(gerenciar_sensor_1, '127.0.0.1', 8888)
+    server_2 = await asyncio.start_server(gerenciar_sensor_2, '127.0.0.1', 8889)
+    server_3 = await asyncio.start_server(gerenciar_sensor_3, '127.0.0.1', 8890)
+
+    async with server_1, server_2, server_3:
         print("Seridor de Ananlise de Sensores Trabalhando...")
-        await server.serve_forever()
+        #await server.serve_forever()
+        await asyncio.gather(
+            server_1.serve_forever(),
+            server_2.serve_forever(),
+            server_3.serve_forever()
+        )
 
 asyncio.run(main())
